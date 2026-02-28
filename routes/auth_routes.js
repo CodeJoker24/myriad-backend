0aecab0734f4d7ebfab5f3b6363e66ee37c9dd44
CodeJoker24 <damilaremishael@gmail.com>
2026-02-27 20:03:40 -0800

    added login function to auth_routes.js

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -31,4 +31,43 @@ router.post("/signup", async(req, res)=>{
 });
 
 
+
+router.post("/login", async(req, res)=>{
+    try{
+        const {email, password} = req.body;
+        const {data:authData, error} = await supabse.auth.signInWithPassword({
+            email, 
+            password
+        })
+
+        if(error){
+            if(error.message == "Email not confirmed"){
+                res.status(400).json({error:"Chech your email for Confirmation"})
+                return;
+            }
+            return res.status(400).json({error:error.message});
+        }
+
+        const {data:tableData} = await supabse.from("myriad_users").select("*")
+        .eq("id", authData.user.id)
+        .single()
+
+        res.json({
+            message:"Login Successful",
+            user:{
+                id:tableData.id,
+                email:tableData.email,
+                name:tableData.name
+            },
+            session:authData.session
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            error:error.message
+        })
+    }
+})
+
+
 module.exports = router;
