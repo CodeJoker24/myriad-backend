a5f7f6ed1eb91506014d7567d88ec32cfbdc69d7
CodeJoker24 <damilaremishael@gmail.com>
2026-07-03 14:21:07 -0700

    added rate limiter to prevent crashes and spam

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const supabase = require("./lib/db");
+const rateLimit = require("express-rate-limit");
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 const authRoutes = require("./routes/auth_routes");
+
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  standardHeaders: true, 
+  legacyHeaders: false, 
+  message: {
+  status: 429,
+  error: "Too many requests from this device. Please try again after 15 minutes."
+  }
+})
+
+app.use(limiter);
 
 app.use(cors());
 app.use(express.json());
